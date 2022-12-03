3439c3cbb09a9251db20c0531e5be2daa923ea1d
Tomás Alves <102360450+Tashmacher@users.noreply.github.com>
2022-12-03 15:38:09 +0000

    Add files via upload

---
+++ pgrepwc_processos.py
@@ -0,0 +1,13 @@
+### Grupo: SO-TI-09
+### Aluno 1: Henrique Venâncio (fc58618)
+### Aluno 2: Laura Tomás (fc58641)
+### Aluno 3: Tomás Alves (fc58633)
+
+import sys
+
+def main(args):
+    print('Programa: pgrepwc_processos.py')
+    print('Argumentos: ',args)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
